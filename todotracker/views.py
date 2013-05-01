3b152ece72ba28faaab98fcac9bfdec22a514071
Vetle Engesbak <vetleengesbak@gmail.com>
2013-05-01 16:40:04 +0200

    working on pagination in template

--- todotracker/views.py
+++ todotracker/views.py
@@ -46,7 +46,7 @@ def viewtodosview (request, pgnumb=1, message=None):
         pgcount = (total_todos_selected-1/todos_per_page)+1
     
     #serve content
-    c = {'todos': todos_to_show, 'pgnumb': pgnumb, "pgcount": pgcount}
+    c = {'todos': todos_to_show, 'pgnumb': pgnumb, 'pgcount': pgcount}
     c.update(csrf(request))
     template = "view_todos.html"
     return render_to_response(template, c, context_instance=RequestContext(request))
